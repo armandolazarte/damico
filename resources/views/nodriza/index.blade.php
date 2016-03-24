7c9dade9207e902d2f35ca896f5a856b1c65c958
Medaglia, Germán <german.medaglia@gmail.com>
2016-03-24 10:31:56 -0300

    Various.

--- resources/views/nodriza/index.blade.php
+++ resources/views/nodriza/index.blade.php
@@ -6,7 +6,14 @@
             <h2>Nodriza</h2>
             <div class="row">
                 <div class="col-md-8">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                    <p>
+                        Fuente de alimentación para 10 pedales, regulada y filtrada para tener el más mínimo nivel de ruido. 
+                        Su reducido tamaño nos asegura lugar en cualquier pedalboard para más pedales. Fácil de transportar, la NODRIZA posee un led indicador de estado y está acompañada por un pequeño transformador y 10 cables con conectores rectos y en ángulo de 2.1mm de distintos largos y polaridad negativa al centro (opcional cambio de polaridad y/o conector).
+                    </p>
+                    <p>
+                        Como si esto fuera poco, la NODRIZA puede alimentar los stompboxes de Line 6 (DL4, MM4, DM4, etc...) sin ningún problema. 
+                        Olvidate de transportar una fuente exclusiva para el DL4 y/o cualquier otro Stompbox Modeler. En fin, <strong>sin la NODRIZA las demás naves no pueden sonar!</strong>
+                    </p>
                     <div class="row">
                         @foreach ($data->img as $i => $img)
                             <div class="col-md-4 col-sm-6">
@@ -22,7 +29,8 @@
                         @endforeach
                     </div>
                     <p>Precio: <big><strong>${{ $data->price }}</strong></big></p>
-                    <p class="mb20"><a href="{{ route('nodriza-order') }}" class="btn btn-large btn-block btn-info">QUIERO LA MÍA!</a></p>
+                    <p>Modelo patentado <span class="glyphicon glyphicon-registration-mark"></span></p>
+                    <p class="mb20 text-center"><a href="{{ route('nodriza-order') }}" class="btn btn-lg btn-block btn-primary">QUIERO LA MÍA!</a></p>
                     
                     @include('fb_comments')
                 </div>
